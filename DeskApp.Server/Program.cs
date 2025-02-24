8f782f4b48aa1edef53bbce67feb4ef7a181d449
AniikaDee <anna.dziembala@gmail.com>
2025-02-24 20:34:40 +0100

    creating admin account on first launch

--- DeskApp.Server/Program.cs
+++ DeskApp.Server/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using DeskApp.Server.Data;
+using DeskApp.Server.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddCors(options =>
@@ -37,7 +38,25 @@ using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<DeskAppDbContext>();
     dbContext.Database.EnsureCreated();
+    if (!dbContext.Users.Any())
+    {
+        dbContext.Users.Add(new User
+        {
+            Id = Guid.NewGuid(),  
+            FirstName = "Admin",
+            LastName = "User",
+            Email = "admin@example.com",
+            Password = "d9b5f58f0b38198293971865a14074f59eba3e82595becbe86ae51f1d9f1f65e",
+            Position = "",
+            Team = "",
+            Access = "ADMIN"  
+        });
+
+        dbContext.SaveChanges();
+    }
 }
+
+
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
