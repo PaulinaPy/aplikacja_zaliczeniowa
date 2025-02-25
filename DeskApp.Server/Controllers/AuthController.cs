46a682d57fe68d5dffb66dcda0592f33930f2f30
AniikaDee <anna.dziembala@gmail.com>
2025-02-25 12:16:07 +0100

    Update AuthController.cs
    Removed "required" members to give C# 10.0 compatibility

--- DeskApp.Server/Controllers/AuthController.cs
+++ DeskApp.Server/Controllers/AuthController.cs
@@ -62,8 +62,8 @@ namespace YourApp.Controllers
     }
     public class Credentials
     {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 
 }
